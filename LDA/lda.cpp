40d866cf9197a20ee694ba104f867d1ead50066f
Marc Fish <zs_fish@126.com>
2020-09-17 22:15:58 +0800

    lda

--- LDA/lda.cpp
+++ LDA/lda.cpp
@@ -8,6 +8,13 @@ boost::random::mt19937 rng;
 void LDA::initModel() {
 	set<int> word_set;
 	for (auto d = doc.begin(); d != doc.end(); d++) {
+		vector<int> temp(d->size(), 0);
+		z.push_back(temp);
+		vector<int> temp2(K, 0);
+		nmk.push_back(temp2);
+		nmkSum.push_back(0);
+		vector<double> temp3(K, 0.0);
+		theta.push_back(temp3);
 		for (auto w = (*d).begin(); w != (*d).end(); w++) {
 			word_set.insert(*w);
 		}
@@ -17,7 +24,7 @@ void LDA::initModel() {
 	cout << "topic number" << K << endl;
 	cout << "doc number" << M << endl;
 	cout << "word number" << V << endl;
-	boost::uniform_int<int> random(0, K);
+	boost::uniform_int<int> random(0, K-1);
 	for (int i = 0; i < K; i++) {
 		vector<int> temp(V, 0);
 		vector<double> temp2(V, 0.0);
@@ -25,22 +32,16 @@ void LDA::initModel() {
 		nktSum.push_back(0);
 		phi.push_back(temp2);
 	}
-	for (auto d = doc.begin(); d != doc.end(); d++) {
-		vector<int> temp(d->size(), 0);
-		z.push_back(temp);
-		vector<int> temp2(K, 0);
-		nmk.push_back(temp2);
-		nmkSum.push_back(0);
-		vector<double> temp3(K, 0.0);
-		theta.push_back(temp3);
-		for (int i = 0; i < d->size(); i++) {
+	cout << "init" << endl;
+	for (int j = 0; j < M;j++) {
+		for (int i = 0; i < doc.at(j).size(); i++) {
 			int initTopic = random(rng);
-			z.back().at(i) = initTopic;
-			nmk.back().at(initTopic)++;
-			nkt.at(initTopic).at(d->at(i))++;
+			z.at(j).at(i) = initTopic;
+			nmk.at(j).at(initTopic)++;
+			nkt.at(initTopic).at(doc.at(j).at(i))++;
 			nktSum.at(initTopic)++;
 		}
-		nmkSum.back() = d->size();
+		nmkSum.at(j) = doc.at(j).size();
 	}
 }
 void LDA::inference() {
