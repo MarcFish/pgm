1feccab81b3c923a6bd4f8320b9f03e5257a6183
Marc Fish <zs_fish@126.com>
2020-09-17 17:13:43 +0800

    lda

--- LDA/main.cpp
+++ LDA/main.cpp
@@ -1,22 +1,47 @@
 #include<iostream>
 #include<string>
-#include"csv.h"
+#include<regex>
+#include<vector>
+#include <iterator>
+#include <fstream>
+#include <sstream>
 #include<boost/program_options.hpp>
 using namespace std;
 namespace po = boost::program_options;
+vector<vector<int>> doc;
+
+std::vector<int> getLine(std::istream& str)
+{
+	std::vector<int>   result;
+	std::string                line;
+	std::getline(str, line);
+
+	std::stringstream          lineStream(line);
+	std::string                cell;
+
+	while (std::getline(lineStream, cell, ','))
+	{
+		result.push_back(stoi(cell));
+	}
+	return result;
+}
+
 int main(int argc, char const* argv[]) {
+	// cmdline parse
 	double alpha;
 	double beta;
 	int iterNumber;
 	int infNumber;
 	int topicNumber;
+	string docfile;
 	po::options_description opt("all options");
 	opt.add_options()
 		("alpha", po::value<double>(&alpha)->default_value(0.1))
 		("beta", po::value<double>(&beta)->default_value(0.1))
 		("iter", po::value<int>(&iterNumber)->default_value(100))
 		("inf", po::value<int>(&infNumber)->default_value(10))
-		("topic", po::value<int>(&topicNumber)->default_value(100));
+		("topic", po::value<int>(&topicNumber)->default_value(100))
+		("file", po::value<string>(&docfile)->default_value("../data/reuter.csv"));
 	po::variables_map vm;
 	try {
 		po::store(parse_command_line(argc, argv, opt), vm);
@@ -25,6 +50,12 @@ int main(int argc, char const* argv[]) {
 		std::cout << "input param wrong!!!" << endl;
 	}
 	po::notify(vm);
-	cout << topicNumber << endl;
-	cout << "hello world" << endl;
+	// read doc file
+	ifstream docStream;
+	docStream.open(docfile);
+	while (!docStream.eof()) {
+		doc.push_back(getLine(docStream));
+	}
+	docStream.close();
+	doc.pop_back();
 }
